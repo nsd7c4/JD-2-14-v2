abc29c65c5b514df0128e099b41ff33e3e875e2f
rashidgafurov <nsd7c4@gmail.com>
2022-07-08 13:26:16 -0400

    Initial commit

---
+++ src/main/java/com/grr/jd214v2/Jd214V2Application.java
@@ -0,0 +1,13 @@
+package com.grr.jd214v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd214V2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jd214V2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/grr/jd214v2/Jd214V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grr.jd214v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jd214V2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
